0887ebeea15abc749bd8cb808fe28dfc676e55f6
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-04-20 17:53:10 -0300

    update

--- respostaData.js
+++ respostaData.js
@@ -3,10 +3,8 @@ import arrayHolidays from "./dados.js";
 
 function respostaArrayData() {
     const holidaysFormat = transformData(arrayHolidays);
-    console.log(holidaysFormat);
     let arrayResposta = [];
     for(let i = 0; i < arrayHolidays.length; i++){
-        console.log(arrayHolidays[i]);
         arrayResposta.push({...arrayHolidays[i], date: holidaysFormat[i]});
     }
     return arrayResposta;
